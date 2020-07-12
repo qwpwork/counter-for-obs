dd0fd6d44e2efdab2424efd9c73264de8b4c4125
Alexander Peremitin <qwpwork@gmail.com>
2020-07-12 20:22:56 +0700

    reactive settings

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ const countBlock = document.getElementById('countValue'),
       resetCount = document.getElementById('countReset'),
       countInput = document.getElementById('countInput'),
       countSetBtn = document.getElementById('countSetBtn'),
+      setTextColor = document.getElementById('inputForTextColor'),
+      setBgColor = document.getElementById('inputForBgColor'),
+      setTitle = document.getElementById('inputForCountTitle'),
       countDecrement = document.getElementById('countDecrement'),
+      countWrapper = document.getElementById('countWrapper'),
+      countDescription = document.getElementById('countDescription'),
       countIncrement = document.getElementById('countIncrement');
 
 
@@ -11,6 +16,7 @@ if (localStorage.getItem('count') == undefined) {
 }
 
 let countValue = parseInt(localStorage.getItem('count'));
+countInput.value = countValue;
 
 countBlock.innerHTML = countValue;
 
@@ -25,11 +31,12 @@ function incrementAndDecrement(option) {
       }
       break;
     default:
-      console.log('error');
+      console.log('unknown operation');
       break;
   }
   localStorage.setItem('count', countValue)
   countBlock.innerHTML = countValue;
+  countInput.value = countValue;
 }
 
 countIncrement.addEventListener("click", function(event) {
@@ -40,24 +47,30 @@ countDecrement.addEventListener("click", function(event) {
   incrementAndDecrement('minus');
 });
 
-countInput.addEventListener("keypress", function(event){
-  if(event.charCode >= 48 && event.charCode <= 57 == false) {
-    event.preventDefault();
-  }
+countInput.addEventListener("input", function(event){
+  countBlock.innerHTML = countInput.value;
+  countValue = parseInt(countInput.value);
+  localStorage.setItem('count', countValue);
 });
 
-countSetBtn.addEventListener("click", function(){
-  if ((countInput.value != "") && (countInput.value >= 0)) {
-    countBlock.innerText = countInput.value;
-    countValue = parseInt(countInput.value);
-    localStorage.setItem('count', countValue)
-  }
-})
+setTextColor.addEventListener("input", function(){
+  countDescription.style.color = setTextColor.value;
+  countBlock.style.color = setTextColor.value;
+});
+
+setBgColor.addEventListener("input", function(){
+  countWrapper.style.background = setBgColor.value;
+});
+
+setTitle.addEventListener("input", function(){
+  countDescription.innerHTML = setTitle.value;
+});
 
 countReset.addEventListener("dblclick", function(){
   if (countValue != 0) {
-    countBlock.innerText = 0;
     countValue = 0;
     localStorage.clear();
+    countBlock.innerText = 0;
+    countInput.value = countValue;
   }
-})
+});
